c8b42b1920aef77326749419b6dbe3c3d488fada
Daniel <danielprdj@gmail.com>
2023-04-26 15:22:52 -0500

    commit 3

---
+++ PubliUcoEntities/src/co/edu/uco/publiuco/entities/TipoRelacionInstitucionEntity.java
@@ -0,0 +1,70 @@
+package co.edu.uco.publiuco.entities;
+
+import java.util.UUID;
+
+import co.edu.uco.publiuco.crosscutting.utils.UtilObject;
+import co.edu.uco.publiuco.crosscutting.utils.UtilText;
+import co.edu.uco.publiuco.crosscutting.utils.UtilUUID;
+
+
+public class TipoRelacionInstitucionEntity {
+	private UUID identificador;
+	private String nombre;
+	private String descripcion;
+	private EstadoTipoRelacionInstitucionEntity estado;
+	
+	
+	
+	public TipoRelacionInstitucionEntity(final UUID identificador, final String nombre, final String descripcion, final EstadoTipoRelacionInstitucionEntity estado) {
+		super();
+		setIdentificador(identificador);
+		setNombre(nombre);
+		setDescripcion(descripcion);
+		setEstado(estado);
+		}
+	
+	
+	
+	public final UUID getIdentificador() {
+		return identificador;
+	}
+	private final void setIdentificador(final UUID identificador) {
+		this.identificador = UtilUUID.getDefault(identificador); 
+		
+	}
+	public final String getNombre() {
+		return nombre;
+	}
+	
+	
+	private final void setNombre(final String nombre) {
+		this.nombre = UtilText.getUtilText().applyTrim(nombre);
+				
+	}
+	
+	public final String getDescripcion() {
+		return descripcion;
+	}
+	
+	
+	private final void setDescripcion(final String descripcion) {
+		this.descripcion = UtilText.getUtilText().applyTrim(nombre);
+		
+	}
+
+
+	public final EstadoTipoRelacionInstitucionEntity getEstado() {
+		return estado;
+	}
+
+
+	private final void setEstado(final EstadoTipoRelacionInstitucionEntity estado) {
+		this.estado = UtilObject.getDefault(estado, EstadoTipoRelacionInstitucionEntity.getDefaultObject());
+		
+	}
+	
+	
+
+
+	
+}
